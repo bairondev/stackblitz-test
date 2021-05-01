bd132cd90eb7bf4654cd6cf5fea77d3ee2e6d110
bairondev <crearestudios@gmail.com>
2021-05-01 00:13:14 -0500

    Testing Stackblitz Tools

--- index.tsx
+++ index.tsx
@@ -21,7 +21,7 @@ class App extends Component<AppProps, AppState> {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          Testing Stackblitz:::::
         </p>
       </div>
     );
